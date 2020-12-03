2859ae70ab43543b5c0020c4377b1100401aa0d3
UMI MADIHAH BT MOHAMED RAIMI <47856883+madihahdihah97@users.noreply.github.com>
2020-12-04 02:24:52 +0800

    Create registration.php

---
+++ registration.php
@@ -0,0 +1,27 @@
+<?php
+    
+    session_start();
+
+$con = mysqli_connect('localhost', 'root', '123456');
+
+mysqli_select_db($con, 'userregistration');
+
+$name = $_POST['user'];
+$pass = $_POST['password'];
+
+$s = " select * from usertable where name = '$name' && password = '$pass'";
+
+$result = mysqli_query ($con, $s);
+
+$num = mysqli_num_rows ($result);
+
+if($num == 1){
+    echo"Username Already Taken!";
+    
+}else{
+    $reg= "insert into usertable(name, password) values ('$name', '$pass')";
+    mysqli_query($con, $reg);
+    echo "Registration Successful!";
+  
+}
+?>
